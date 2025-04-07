97cb92e4e5a52aae5ef277a13e4a8b4f43e84c31
hghest <gbadebosamuel23@gmail.com>
2025-04-06 20:11:32 -0700

    done

--- components/Submit.jsx
+++ components/Submit.jsx
@@ -13,7 +13,13 @@ const Submit = () => {
                     <h4>Feedback</h4>
             </div>
                 <div className='col-md-6 col-12'>
-                   <img src="https://s3-alpha-sig.figma.com/img/c414/1931/39d0cf05bdd45bf7d4b56e273bfe7cd0?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=XKPKStFOesm3dD3aeZyfGqUQrVCVPWeEGi4ccJuy-FKQsTmZokVeqIG925tkWFEfex0Of3eOkH7FUbsOupnXLm3Ao26foPJC-72VoOojRzAJJx-HeOaRFKa6pTOlTC7Mg1k5oJUr4kqA9PD08odOaIB6jjQTn6KOVqSZnRGJyHFIMeZmxaLTxWwRQsEPoCP6sPcwJzKH3fr2aJSkBq27olk4s8pr30UhupVAyfmiarqhIsDHuNzmAqFlbc02inNJrEuDTdW5FwxNudUvLPRgF1fTtw5Y5gXFKQ-rPwomMatAylDEjOssKhbBo4jYAw6Q1xwX1ZO6StNHAfHIHTcbTg__" alt="" className='pic' srcset="" />
+                   <img 
+                     src="https://s3-alpha-sig.figma.com/img/c414/1931/39d0cf05bdd45bf7d4b56e273bfe7cd0?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=XKPKStFOesm3dD3aeZyfGqUQrVCVPWeEGi4ccJuy-FKQsTmZokVeqIG925tkWFEfex0Of3eOkH7FUbsOupnXLm3Ao26foPJC-72VoOojRzAJJx-HeOaRFKa6pTOlTC7Mg1k5oJUr4kqA9PD08odOaIB6jjQTn6KOVqSZnRGJyHFIMeZmxaLTxWwRQsEPoCP6sPcwJzKH3fr2aJSkBq27olk4s8pr30UhupVAyfmiarqhIsDHuNzmAqFlbc02inNJrEuDTdW5FwxNudUvLPRgF1fTtw5Y5gXFKQ-rPwomMatAylDEjOssKhbBo4jYAw6Q1xwX1ZO6StNHAfHIHTcbTg__" 
+                     alt="" 
+                     className='pic' 
+                     style={{ width: '578px', height: '318px' }} 
+                     srcset="" 
+                   />
                 </div>
             </div>
         </div>
